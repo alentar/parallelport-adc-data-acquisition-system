a5600ac81ad01df164cbadefd4c43f223698b47e
Kasun Vithanage <alankasun@gmail.com>
2019-01-21 16:34:30 +0530

    basic connection

--- parallelportmonitor/src/main/java/org/alentar/parallelportmon/controllers/MainController.java
+++ parallelportmonitor/src/main/java/org/alentar/parallelportmon/controllers/MainController.java
@@ -1,58 +1,89 @@
 package org.alentar.parallelportmon.controllers;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
-import org.alentar.parallelportmon.components.GraphTab;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import org.alentar.parallelportmon.dialogs.CommonDialogs;
+import org.alentar.parallelportmon.dialogs.ConnectionDialog;
 import org.alentar.parallelportmon.manager.ResourceManager;
 import org.alentar.parallelportmon.stream.StreamManager;
 import org.alentar.parallelportmon.tcp.ParaMonClient;
 
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 
 public class MainController {
+    final Image connectedImage = new Image("icons/icons8-connected-100.png");
     public Button btnConnect;
+    final Image disconnectedImage = new Image("icons/icons8-disconnected-100.png");
+    final Color RED = new Color(1, 0, .1, 1);
+    final Color GREEN = new Color(.2, 1, 0, 1);
     public Label lblStatus;
+
     public TabPane tabPane;
-    StreamManager streamManager;
+    public ImageView connectButtonImageView;
+    public Circle connectionIndicator;
+
+
 
     private boolean isConnected = false;
 
     @FXML
-    private void initialize(){
-        updateStatus(isConnected);
-        tabPane.getTabs().add(new GraphTab(0, "Channel 0", "Sensor 0", "Time/s", "Voltage/v"));
-        tabPane.getTabs().add(new GraphTab(1, "Channel 1", "Sensor 1", "Time/s", "Voltage/v"));
-        tabPane.getTabs().add(new GraphTab(3, "Channel 3", "Sensor 3", "Time/s", "Voltage/v"));
+    private void initialize() {
 
-        try {
-            ParaMonClient paraMonClient = new ParaMonClient("127.0.0.1", 2335);
-            streamManager = new StreamManager(paraMonClient);
-            ResourceManager.getInstance().setStreamManager(streamManager);
-
-            streamManager.scheduleChannelStream(0, 1, 1, TimeUnit.SECONDS);
-            streamManager.scheduleChannelStream(1, 2, 5, TimeUnit.SECONDS);
-            streamManager.scheduleChannelStream(3, 2, 5, TimeUnit.MILLISECONDS);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
     }
 
-    private void updateStatus(boolean isConnected){
-        String txt = (isConnected ? "Connected" : "Disconnected");
-        lblStatus.setText(txt);
+    private void updateStatusLabel(boolean isConnected) {
+        lblStatus.setText(isConnected ? "Connected" : "Disconnected");
+        connectionIndicator.setFill(isConnected ? GREEN : RED);
+    }
+
+    private void updateConnectButton(boolean isConnected) {
+        btnConnect.setText(isConnected ? "Disconnect" : "Connect");
+        connectButtonImageView.setImage(isConnected ? disconnectedImage : connectedImage);
     }
 
     public void connect(ActionEvent actionEvent) {
-        isConnected = !isConnected;
-        String txt = (isConnected? "Disconnect" : "Connect");
-        updateStatus(isConnected);
-        btnConnect.setText(txt);
-        streamManager.cancelChannelStream(0);
+        if (!isConnected) {
+            ConnectionDialog connectionDialog = new ConnectionDialog();
+            connectionDialog.showAndWait().ifPresent(connectionData -> {
+                try {
+                    ParaMonClient paraMonClient = new ParaMonClient(connectionData.getHost(), connectionData.getPort());
+                    StreamManager streamManager = new StreamManager(paraMonClient);
+                    ResourceManager.getInstance().setStreamManager(streamManager);
+                    isConnected = true;
+                } catch (Exception ex) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Connection error");
+                    alert.setContentText(String.format("Error connecting to %s:%d", connectionData.getHost(), connectionData.getPort()));
+                    alert.showAndWait();
+                    isConnected = false;
+                }
+            });
+        } else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you really want to disconnect from data server ?\nAll streams will be halted!", ButtonType.NO, ButtonType.YES);
+            alert.setTitle("Disconnect from Data Server");
+            alert.showAndWait().ifPresent(buttonType -> {
+                if (buttonType == ButtonType.YES) {
+                    try {
+                        StreamManager streamManager = ResourceManager.getInstance().getStreamManager();
+                        if (streamManager != null) {
+                            streamManager.close();
+                        }
+                        isConnected = false;
+                    } catch (IOException ex) {
+                        CommonDialogs.ExceptionAlert(ex).showAndWait();
+                        Platform.exit();
+                    }
+                }
+            });
+        }
+
+        updateStatusLabel(isConnected);
+        updateConnectButton(isConnected);
     }
-
-
 }
---
+++ parallelportmonitor/src/main/java/org/alentar/parallelportmon/dialogs/ConnectionDialog.java
@@ -0,0 +1,64 @@
+package org.alentar.parallelportmon.dialogs;
+
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+
+public class ConnectionDialog extends Dialog<ConnectionData> {
+    public ConnectionDialog() {
+        setTitle("Open new connection");
+        setHeaderText("Connect to Alentar Parellel Port Data Server");
+
+        ButtonType connectButtonType = new ButtonType("Connect", ButtonBar.ButtonData.OK_DONE);
+        getDialogPane().getButtonTypes().addAll(connectButtonType, ButtonType.CANCEL);
+
+        // Create the username and password labels and fields.
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField host = new TextField();
+        host.setPromptText("Set host name (default: 127.0.0.1)");
+        host.setText("127.0.0.1");
+        TextField port = new TextField();
+        port.setPromptText("Port for connection (2335 will be used if empty)");
+        port.setText("2335");
+
+        grid.add(new Label("Host:"), 0, 0);
+        grid.add(host, 1, 0);
+        GridPane.setHgrow(host, Priority.ALWAYS);
+        grid.add(new Label("Port:"), 0, 1);
+        grid.add(port, 1, 1);
+
+
+        getDialogPane().setContent(grid);
+        Platform.runLater(host::requestFocus);
+
+        setResultConverter(buttonType -> {
+            if (buttonType == connectButtonType) {
+                String hostData = "127.0.0.1";
+                int portData = 2335;
+
+                if (!host.getText().trim().isEmpty()) hostData = host.getText();
+
+                try {
+                    if (!port.getText().trim().isEmpty())
+                        portData = Integer.parseInt(port.getText().trim());
+                } catch (NumberFormatException ex) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid port value");
+                    alert.setContentText("Port should be an integer");
+                    alert.showAndWait();
+                    return null;
+                }
+
+                return new ConnectionData(hostData, portData);
+            }
+
+            return null;
+        });
+    }
+}
